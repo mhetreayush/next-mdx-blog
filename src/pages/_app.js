7a740b7fb3721aff93fb7c8cbfda0597a7b7eab9
Ayush <mhetreayush1719@gmail.com>
2023-01-15 21:59:43 +0530

    vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import Head from "next/head";
 import Navbar from "../Components/Navbar";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }) {
           <Navbar />
           <main>
             <Component {...pageProps} />
+            <Analytics />
           </main>
         </div>
       </div>
